b9ccc4670fb9fc490a379ee26bd1f5a1830bd051
Dwayne Redwood <dwayneredwood@live.com>
2022-12-17 18:18:19 -0500

    modified dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -6,4 +6,5 @@ yum install curl-devel expat-devel gettext-devel openssl-devel zlib-devel perl-C
 echo "Installing libraries for compiler-enforced C99 requirements";
 yum install centos-release-scl -y;
 yum install devtoolset-7-gcc* -y;
+yum clean all -y
 scl enable devtoolset-7 bash;
