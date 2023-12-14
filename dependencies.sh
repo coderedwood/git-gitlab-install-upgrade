6e50762f0bbfde193b7e095d8320391cb208e497
Dwayne Redwood <dwayneredwood@live.com>
2023-12-14 04:32:00 -0500

    updated and tested on centos8

--- dependencies.sh
+++ dependencies.sh
@@ -1,9 +1,5 @@
 ##########This is a script to install git from source on CentOS 7 / RedHat 7 ##############;
 echo "Installing Dependencies";
 yum groupinstall 'Development Tools' -y;
-yum install curl-devel expat-devel gettext-devel openssl-devel zlib-devel perl-CPAN perl-devel wget -y;
-echo "Installing libraries for compiler-enforced C99 requirements";
-yum install centos-release-scl -y;
-yum install devtoolset-7-gcc* -y;
+yum install curl-devel expat-devel gettext-devel openssl-devel zlib-devel perl-CPAN perl-devel wget gcc autoconf -y;
 yum clean all;
-scl enable devtoolset-7 bash;
