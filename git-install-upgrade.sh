cc25c2de3826e834ac987a588852e2f2df5b596d
Dwayne Redwood <dwayneredwood@live.com>
2024-04-02 07:12:46 -0500

    updated script for tag fetching using github API and curl

--- git-install-upgrade.sh
+++ git-install-upgrade.sh
@@ -1,13 +1,16 @@
 ##########This is a script to install git from source on CentOS 7 / RedHat 7 ##############
 # Uncomment and comment accordingly lines 4 & 5 while lines 7 - 11 for version specific download
-# Lines 4 & 5 pulls a larger directory vs pulling a release tag
+# Line 13 pulls a larger directory vs pulling a release tag via lines 5-7
+####################### Get Git via release tag via http #########################################
+# TAG=$(curl -s https://api.github.com/repos/git/git/tags | grep -i "name" | awk -F '"' '{print $4}' | head -n 1)
+# echo "Fetching the latest git release"
+# curl -OJL "https://github.com/git/git/archive/refs/tags/${TAG}.tar.gz"
+# echo "Extracting tar file";
+# tar xzfv v*.tar.gz;
+# rm -rf v*tar.gz
+####################### Build Git from Source Section ############################################
 echo "cloning git project from master repository since base git is installed from dependecnies";
 git clone https://github.com/git/git.git;
-# echo "Fetching the latest git release";
-# wget https://github.com/git/git/archive/refs/tags/v2.39.0.tar.gz;
-# echo "Extracting tar file";
-# tar xzf v*.tar.gz;
-# rm -rf v*tar.gz
 echo "Changing Diractory";
 cd git*/;
 echo "Building from source files";
