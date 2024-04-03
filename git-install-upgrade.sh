c30c788bf015ecca3e38fe9d61622a205c99d2d6
Dwayne Redwood <dwayneredwood@live.com>
2024-04-03 00:52:05 -0500

    changed execution to functions

--- git-install-upgrade.sh
+++ git-install-upgrade.sh
@@ -2,18 +2,32 @@
 # Uncomment and comment accordingly lines 5-10 or lines 12 - 13 for version specific source
 # Line 13 pulls a larger directory vs pulling a release tag via lines 5-7
 ####################### Get Git via release tag via http #########################################
-# TAG=$(curl -s https://api.github.com/repos/git/git/tags | grep -i "name" | awk -F '"' '{print $4}' | head -n 1)
-# echo "Fetching the latest git release"
-# curl -L "https://github.com/git/git/archive/refs/tags/${TAG}.tar.gz" > "git-${TAG}.tar.gz"
-# echo "Extracting tar file";
-# tar -xzfv git*.tar.gz;
-# rm -rf git*tar.gz
+download_git(){
+TAG=$(curl -s https://api.github.com/repos/git/git/tags | grep -i "name" | awk -F '"' '{print $4}' | head -n 1)
+echo "Fetching the latest git release"
+curl -L "https://github.com/git/git/archive/refs/tags/${TAG}.tar.gz" > "git-${TAG}.tar.gz";
+}
+extract_git(){
+echo "Extracting tar file";
+tar -xzfv git*.tar.gz;
+rm -rf git*tar.gz
+}
 ####################### Build Git from Source Section ############################################
+clone_git(){
 echo "cloning git project from master repository since base git is installed from dependecnies";
 git clone https://github.com/git/git.git;
+}
+install_git(){
 echo "Changing Diractory";
 cd git*/;
 echo "Building from source files";
 make configure;
 ./configure --prefix=/usr/local;
 sudo make all && sudo make install;
+}
+
+#Main routine
+download_git;
+extract_git;
+#clone_git;
+install_git;
